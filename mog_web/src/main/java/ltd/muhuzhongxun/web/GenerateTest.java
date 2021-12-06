5f35770afde0d12886aaa839f35eef64063b3514
muhuzhongxun <983724494@qq.com>
2021-12-06 17:17:45 +0800

    初建多模块

---
+++ mog_web/src/main/java/ltd/muhuzhongxun/web/GenerateTest.java
@@ -0,0 +1,50 @@
+package ltd.muhuzhongxun.web;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+
+public class GenerateTest {
+    public static void main(String[] args) {
+        //创建generator对象
+        AutoGenerator autoGenerator = new AutoGenerator();
+        //数据源
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL);
+        dataSourceConfig.setDriverName("com.mysql.cj.jdbc.Driver");
+        dataSourceConfig.setUsername("root");
+        dataSourceConfig.setPassword("123456");
+        dataSourceConfig.setUrl("jdbc:mysql://localhost:3306/manual_og_system");
+        autoGenerator.setDataSource(dataSourceConfig);
+        //全局配置
+        GlobalConfig globalConfig = new GlobalConfig();
+        globalConfig.setOutputDir(System.getProperty("user.dir") + "/src/main/java");
+        globalConfig.setAuthor("admin");
+        globalConfig.setOpen(true);
+        globalConfig.setServiceName("%sService");
+        autoGenerator.setGlobalConfig(globalConfig);
+        //包信息
+        PackageConfig packageConfig = new PackageConfig();
+        packageConfig.setParent("ltd.muhuzhongxun.web");
+        packageConfig.setEntity("entity");
+        packageConfig.setMapper("mapper");
+        packageConfig.setService("service");
+        packageConfig.setServiceImpl("service.impl");
+        packageConfig.setController("controller");
+        autoGenerator.setPackageInfo(packageConfig);
+        //策略配置
+        StrategyConfig strategyConfig = new StrategyConfig();
+            //选择生成的对象表名，可输入多个"emp","student","teacher","task","essay","judge","choicetask"
+        strategyConfig.setInclude("sys_user");
+        strategyConfig.setNaming(NamingStrategy.underline_to_camel);
+        strategyConfig.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategyConfig.setEntityLombokModel(true);
+        autoGenerator.setStrategy(strategyConfig);
+        //运行
+        autoGenerator.execute();
+    }
+}
